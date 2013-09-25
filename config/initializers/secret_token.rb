4ad79e4b924ccc4c3154ab22b6aecd8ebcddee37
ben2pop <r.bendenoun@gmail.com>
2013-09-25 15:29:18 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Midi::Application.config.secret_token = 'b14c65ad2a0fc2d06e9b95dd8b73f5507f9cf0a1eb8da39c0b4744dcec2a554a1531de12ce1cf4c142cb95f42dbefc1e685c402580e85cf92fd6869b182ccbae'
